52fd9d5b784b40ef9da46aec78ae58b9140c82e6
yodit93 <yoditabebe9391@gmail.com>
2025-05-23 16:06:34 +0300

    feat: used clipboard api for copying functionality

--- app/components/shared/ReferralBanner.tsx
+++ app/components/shared/ReferralBanner.tsx
@@ -1,5 +1,5 @@
 // app/components/shared/ReferralBanner.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ReferralBanner.module.css';
 import { Gift } from 'lucide-react'; // Example icon
 
@@ -10,9 +10,20 @@ interface ReferralBannerProps {
   onButtonClick?: () => void;
 }
 
-const ReferralBanner: React.FC<ReferralBannerProps> = ({ title, description, buttonText, onButtonClick }) => {
+const ReferralBanner: React.FC<ReferralBannerProps> = ({ title }) => {
+  const [tooltipText, setTooltipText] = useState("Click to copy");
+
+  const handleReferralClick = () => {
+    // copy the referal link when clicked
+    navigator.clipboard.writeText("Referrral link copied to clipboard");
+      setTooltipText("Copied!");
+      setTimeout(() => setTooltipText("Click to copy"), 2000);
+      console.log("Referral link copied to clipboard");
+      // alert('Referral link copied to clipboard!');
+  };
+
   return (
-    <div className={styles.banner}>
+    <button className={`${styles.banner} ${styles.button}`} onClick={handleReferralClick}>
       <div className={styles.iconWrapper}>
         <Gift size={24} />
       </div>
@@ -20,12 +31,15 @@ const ReferralBanner: React.FC<ReferralBannerProps> = ({ title, description, but
         <h4 className={styles.title}>{title || "Refer a Business"}</h4>
         {/* <p className={styles.description}>{description || "And earn £20 when they make their first hire!"}</p> */}
       </div>
+       <span className={styles.tooltip}>
+          {tooltipText}
+        </span>
       {/* {onButtonClick && buttonText && (
         <button onClick={onButtonClick} className={styles.button}>
           {buttonText}
         </button>
       )} */}
-    </div>
+    </button>
   );
 };
 export default ReferralBanner;
