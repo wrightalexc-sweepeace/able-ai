997905ff213686e087c8dc32ffd10600d20ddc93
yodit93 <yoditabebe9391@gmail.com>
2025-05-24 16:52:09 +0300

    featr: add header & text area in chatbot component

--- app/components/onboarding/ChatBotLayout.tsx
+++ app/components/onboarding/ChatBotLayout.tsx
@@ -1,5 +1,8 @@
 import React, { ReactNode } from 'react';
 import styles from './ChatBotLayout.module.css';
+import Logo from '../brand/Logo';
+// import InputBubble from './InputBubble';
+import TextAreaBubble from './TextAreaBubble';
 
 interface ChatBotLayoutProps {
   children: ReactNode;
@@ -12,9 +15,15 @@ const ChatBotLayout = React.forwardRef<HTMLDivElement, ChatBotLayoutProps>(
   ({ children, onScroll, tag, className }, ref) => { // Destructure className
     return (
       <div className={`${styles.chatContainerWrapper} ${className}`}> {/* Apply className */}
-        {tag && <div className={styles.chatTag}>{tag}</div>} {/* Render the tag */}
+        {/* {tag && <div className={styles.chatTag}>{tag}</div>} Render the tag */}
         <div className={styles.chatContainer} onScroll={onScroll} ref={ref}>
+          <div className={styles.header}>
+            <span className={styles.headerText}>Chat with me, Able</span>
+            <Logo width={65} height={65} />
+          </div>
           <div className={styles.chatContent}>{children}</div>
+          {/* <InputBubble placeholder='Chat with Able here ...'/> */}
+          <TextAreaBubble placeholder='Chat with Able here ...'/>
         </div>
       </div>
     );
