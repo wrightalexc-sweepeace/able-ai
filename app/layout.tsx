e644622f7f8e401b67e8c5b35ebea4768f6e869c
yodit93 <yoditabebe9391@gmail.com>
2025-05-27 10:53:12 +0300

    fix: add view ort meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "AbleAI",
   description: "Connecting gig workers and buyers",
+  viewport: 'width=device-width, initial-scale=1',
 };
 
 export default async function RootLayout({
@@ -25,8 +14,8 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-          {children}
+      <body>
+        {children}
       </body>
     </html>
   );
