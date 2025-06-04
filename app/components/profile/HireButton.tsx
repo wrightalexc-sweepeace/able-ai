8dfd67dfe5a5934e70ac8c4b3cc00fda0e5bc403
yodit93 <yoditabebe9391@gmail.com>
2025-06-04 13:20:07 +0300

    feat: create hire button component

---
+++ app/components/profile/HireButton.tsx
@@ -0,0 +1,22 @@
+import { useRouter } from 'next/navigation';
+import styles from './HireButton.module.css';
+import { useAppContext } from '@/app/hooks/useAppContext';
+
+const HireButton = ({workerName, workerId}: {workerName: string, workerId: string}) => {
+    const router = useRouter();
+    const { user: authUser } = useAppContext();
+    const handleHireWorker = () => {
+    if (!workerName || !authUser?.uid) return; // Ensure authUser is available for booking
+    router.push(`/user/${authUser.uid}/buyer/book-gig?workerId=${workerId}`);
+  };
+
+  return (
+    <div className={styles.footerActionBar}>
+        <button onClick={handleHireWorker} className={styles.primaryActionButton}>
+            <span>£</span>Hire {workerName.split(' ')[0]}
+        </button>
+    </div>
+  )
+}
+
+export default HireButton
