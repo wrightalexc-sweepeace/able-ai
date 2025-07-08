83f6ca673e1fa2aa989dfdf6da93173f48f3878f
Jose Tovar <jtovar26455462@gmail.com>
2025-07-08 12:31:26 -0400

    chore: fix typo

--- actions/events/get-calendar-events.ts
+++ actions/events/get-calendar-events.ts
@@ -113,7 +113,7 @@ export async function getCalendarEvents({ userId, role, isViewQA }: { userId: st
     return { events: calendarEvents };
 
   } catch (error: any) {
-    console.error("Error registering user:", error);
+    console.error("Error fetching events:", error);
     return { error: error.message, events: [], status: 500 };
   }
 }
