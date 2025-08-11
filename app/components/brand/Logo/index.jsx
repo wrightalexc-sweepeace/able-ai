5afd5c075e6112fbc7a8058b7928e18701628e4c
yodit93 <yoditabebe9391@gmail.com>
2025-08-11 15:50:35 +0300

    feat: use reusable header component on multiple pages

--- app/components/brand/Logo/index.jsx
+++ app/components/brand/Logo/index.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import Image from 'next/image';
 import styles from './Logo.module.css';
+// import Link from 'next/link';
+// import { useAuth } from '@/context/AuthContext';
 
 const Logo = ({ width = 260, height = 260 }) => {
+  // const { user } = useAuth();
+
   return (
-    <Image
-      src="/images/ableai.png"
-      alt="App Logo"
-      width={width}
-      height={height}
-      className={styles.logo}
-    />
+    // <Link href={`/user/${user?.uid}/able-ai`} className={styles.logoLink}>
+      <Image
+        src="/images/ableai.png"
+        alt="App Logo"
+        width={width}
+        height={height}
+        className={styles.logo}
+      />
+    // </Link>
   );
 };
 export default Logo;
