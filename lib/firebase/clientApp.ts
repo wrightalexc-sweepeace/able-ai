5137a2af862d071fb82cf0860b9432761dfde57e
Anthony Rosales <israellaguan@gmail.com>
2025-08-12 09:43:44 -0500

    feat: add email verification after signup

--- lib/firebase/clientApp.ts
+++ lib/firebase/clientApp.ts
@@ -38,6 +38,14 @@ const storage = getStorage(firebaseApp);
 // Initialize the Gemini Developer API backend service
 const ai = getAI(firebaseApp, { backend: new GoogleAIBackend() });
 
+export const actionCodeSettings = {
+  // URL you want to redirect back to. The domain (www.example.com) for this
+  // URL must be in the authorized domains list in the Firebase Console.
+  url: 'https://www.example.com/finishSignUp?cartId=1234',
+  // This must be true.
+  handleCodeInApp: true,
+};
+
 export const messaging = () => getMessaging(firebaseApp);
 
 export { firebaseApp, authClient, storage, firebaseConfig, ai, db };
