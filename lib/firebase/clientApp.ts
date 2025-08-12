8de21108baeb1e745d1929c09af9178160155833
Anthony Rosales <israellaguan@gmail.com>
2025-08-12 10:59:26 -0500

    feat: email verification with Firebase

--- lib/firebase/clientApp.ts
+++ lib/firebase/clientApp.ts
@@ -41,7 +41,7 @@ const ai = getAI(firebaseApp, { backend: new GoogleAIBackend() });
 export const actionCodeSettings = {
   // URL you want to redirect back to. The domain (www.example.com) for this
   // URL must be in the authorized domains list in the Firebase Console.
-  url: 'https://www.example.com/finishSignUp?cartId=1234',
+  url: window.location.origin + "/usermgmt",
   // This must be true.
   handleCodeInApp: true,
 };
