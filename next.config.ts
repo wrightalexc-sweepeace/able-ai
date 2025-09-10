3bb800eacf45626ed15b7e2a54d6061df94fd860
Anthony Rosales <israellaguan@gmail.com>
2025-09-09 22:33:49 -0500

    fix: cloudflare deploy

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,3 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
-// Here we use the @cloudflare/next-on-pages next-dev module to allow us to
-// use bindings during local development (when running the application with
-// `next dev`). This function is only necessary during development and
-// has no impact outside of that. For more information see:
-// https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
-setupDevPlatform().catch(console.error);
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -16,7 +7,12 @@ const nextConfig: NextConfig = {
         hostname: 'firebasestorage.googleapis.com',
       },
     ],
-  }
+  },
+  output: 'standalone',
+  serverExternalPackages: ["jose"]
 };
 
 export default nextConfig;
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
